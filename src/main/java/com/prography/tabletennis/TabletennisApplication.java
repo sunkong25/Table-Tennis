985a14aaa90720c7536c64dd2e694156331e39e1
sunkong25 <kcindy325k@kyonggi.ac.kr>
2025-02-20 01:31:43 +0900

    [FEAT] Initialize project

---
+++ src/main/java/com/prography/tabletennis/TabletennisApplication.java
@@ -0,0 +1,13 @@
+package com.prography.tabletennis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletennisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletennisApplication.class, args);
+	}
+
+}
