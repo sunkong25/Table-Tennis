21e306ec77216ede0c2865be0c70f82c7788b0fa
sunkong25 <kcindy325k@kyonggi.ac.kr>
2025-02-20 04:11:20 +0900

    [FEAT] Create UserRoom Entity

---
+++ src/main/java/com/prography/tabletennis/domain/room/entity/UserRoom.java
@@ -0,0 +1,40 @@
+package com.prography.tabletennis.domain.room.entity;
+
+import com.prography.tabletennis.domain.room.enums.Team;
+import com.prography.tabletennis.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Builder
+public class UserRoom {
+	@Id
+	@GeneratedValue
+	private int id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "room_id")
+	private Room room;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Enumerated(EnumType.STRING)
+	private Team team;
+}
---
+++ src/main/java/com/prography/tabletennis/domain/room/enums/Team.java
@@ -0,0 +1,5 @@
+package com.prography.tabletennis.domain.room.enums;
+
+public enum Team {
+	RED, BLUE
+}
